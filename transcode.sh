ce6b7694ee335c0dd36408d2a99b5a9af48f7da5
ehime <dodomeki@gmail.com>
2014-02-20 11:31:27 -0800

    Adding test files and updating tanscoder.sh to iterate

--- transcode.sh
+++ transcode.sh
@@ -5,3 +5,17 @@
 # MOD : 2014-02-20 @ 10:51:25
 # VER : Beta 1
 
+# proto: cd drop ; types=(asf asx avi flv m4v mov mp4 mpg rm swf vob wmv); for i in "${types[@]}"; do touch "video.${i}"; done;
+
+# dectypes
+declare -r fpath=$( cd "$(dirname "$0")/drop" ; pwd -P )
+declare -r types="asf\|asx\|avi\|flv\|m4v\|mov\|mp4\|mpg\|ogg\|rm\|swf\|vob\|webm\|wmv"
+declare -r allow="mp4 ogg webm"r
+
+	cd $fpath 
+
+	for video in $(find . -type f -iregex ".*\(${types}\)" -printf '%P\0 '); do
+
+		echo -e "$video"
+
+	done
