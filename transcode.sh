b6249605bea2442e18792307f4c2893e39dba1d0
ehime <dodomeki@gmail.com>
2014-02-24 13:05:01 -0800

    Swapping loop style and moving away from parallel

--- transcode.sh
+++ transcode.sh
@@ -17,10 +17,16 @@
         }
     done
 
+    monitor ()
+    {
+        pid="$1"
+
+
+    }
 
     ## dectypes
     declare -r fpath=$( cd "$(dirname "$0")" ; pwd -P )
-    declare -r types="asf\|asx\|avi\|flv\|m4v\|mov\|mp4\|mpg\|ogg\|rm\|swf\|vob\|webm\|wmv"
+    declare -r types="asf\|asx\|avi\|flv\|m4v\|mkv\|mov\|mp4\|mpg\|ogg\|rm\|swf\|vob\|webm\|wmv"
     declare -r allow=( mp4 ogg webm )
     ##
 
@@ -28,15 +34,15 @@
             [ -d "$fpath/$dir" ] || { mkdir -p "$fpath/$dir"; } # create if not available
         done
 
-            [ -z "$(ls $fpath/$inp)" ] && { "$fpath is empty, finished..."; exit 0; } # exit when empty
+        [ -z "$(ls $fpath/$inp)" ] && { "$fpath is empty, finished..."; exit 0; } # exit when empty
 
 
         ## start video conversion
-        cd $fpath ; for video in $(find . -type f -iregex ".*\(${types}\)" -printf '%P\0 '); do
+        cd $fpath ; while IFS= read -r video; do
 
             skip=0 #reset skip flag
-            name=$(echo $video |awk -NF '.' '{print $1}')
-            exts=$(echo $video |awk -NF '.' '{print $2}')
+            name=$(echo "$video" |awk -NF '.' '{print $1}')
+            exts=$(echo "$video" |awk -NF '.' '{print $2}')
 
             # assign predetermined pos
             for (( i = 0; i < ${#allow[@]}; i++ )); do
@@ -45,15 +51,38 @@
                fi
             done
 
-            cmds=() # empty
-            [[ $skip = 1 ]] || { cmds+=("'$video' -b 1500k -vcodec libx264   -vpre   slow      -vpre baseline -g 30          '$name.mp4' ") ; }
-            [[ $skip = 2 ]] || { cmds+=("'$video' -b 1500k -vcodec libvpx    -acodec libvorbis -ab   160000   -f webm -g 30  '$name.webm'") ; }
-            [[ $skip = 3 ]] || { cmds+=("'$video' -b 1500k -vcodec libtheora -acodec libvorbis -ab   160000   -g 30          '$name.ogg' ") ; }
+            pids=()
+            [[ $skip = 1 ]] ||
+            {
+                ffmpeg -y -i "$inp/$video" -vb 1500k -vcodec libx264 -vpre slow -vpre baseline -g 30 "$out/${name}.mp4" &
+                pids+="$! "
+            }
 
-            echo "${cmds[@]}" | parallel --gnu -j10 ffmpeg -i $fpath/$inp/{};
+            [[ $skip = 2 ]] ||
+            {
+                ffmpeg -y -i "$inp/$video" -vb 1500k -vcodec libvpx -acodec libvorbis -ab 160000 -f webm -g 30 "$out/${name}.webm" &
+                pids+="$! "
+            }
 
-            for ext in "${allow[@]}"; do
-                [ -a "${fpath}/${inp}/${name}.{ext}" ] && mv "${fpath}/${inp}/${name}.{ext}" "${fpath}/${out}/"
+            [[ $skip = 3 ]] ||
+            {
+                ffmpeg -y -i "$inp/$video" -vb 1500k -vcodec libtheora -acodec libvorbis -ab 160000 -g 30 "$out/${name}.ogg" &
+                pids+="$! "
+            }
+
+            for job in `jobs -p`; do
+                echo "Job: $job"
+                wait $job || let "FAIL+=1"
             done
 
-        done
+
+            [ "0" == "$FAIL" ] && echo "YAY!" || echo "FAIL! ($FAIL)"
+
+
+            for ext in "${allow[@]}"; do
+                [ -a "$inp/$name.$ext" ] && cp "$inp/$name.$ext" "$out/"
+            done
+
+        done <<< $(find "$fpath/$inp" -type f -iregex ".*\(${types}\)" -printf '%P\0 ')
+
+    exit
